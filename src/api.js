0a3acdb9df1f5b49f80bd3f2e20cb040d42ef7eb
Linus Lee <100479lee@gmail.com>
2018-03-27 09:12:01 -0400

    Add documentation for the API interfaceg

--- src/api.js
+++ src/api.js
@@ -4,6 +4,18 @@ const { HTTPCache } = require('./utils.js');
 const RHOOD_HTTPS_ROOT = 'https://api.robinhood.com';
 const API_GET_CACHE = new HTTPCache();
 
+/**
+ * @function apiget
+ *
+ * Generic API that interfaces with a Django Rest Framework-based REST API
+ *  with native promises and a cache backend using the custom HTTPCache.
+ *
+ * @param {string} root - the root uri of the API, including the protocol prefix, up to the TLD
+ * @param {string} path - path of the particular API call, from the `/` to the `/`; optional
+ * @param {Object} params - any query parameters for the GET request, formatted as a dictionary / object; optional; values for the dictionary may also be a list of strings or numbers
+ *
+ * @returns {Promise} - a JSON deserialized body of the REST API response
+ */
 const apiget = (root, path = '', params = {}) => {
     const serializedParams = Object.entries(params)
         .map(pair => pair.map(c => encodeURI(c)).join('='))
@@ -41,6 +53,16 @@ const apiget = (root, path = '', params = {}) => {
 
 const api = {
     get: apiget,
+
+    /**
+     * @function api.quote
+     *
+     * Get current quote information for a single ticker / stock.
+     *
+     * @param {string} ticker - the symbol for the security / stock
+     *
+     * @returns {Promise} - deserialized API response to the request
+     */
     quote: ticker => {
         if (typeof ticker === string) {
             return apiget(RHOOD_HTTPS_ROOT, `/quotes/${ticker.toUpperCase()}`);
@@ -48,6 +70,16 @@ const api = {
             throw new Error('Ticker supplied to api.quote must be a string.');
         }
     },
+
+    /**
+     * @function api.quotes
+     *
+     * Get current quote information for multiple tickers / stocks.
+     *
+     * @param {Array:string} tickers - the array of string symbols for securities
+     *
+     * @returns {Promise} - deserialized API response to the request
+     */
     quotes: tickers => {
         if (tickers instanceof Array) {
             return apiget(RHOOD_HTTPS_ROOT, '/quotes/', {
@@ -57,6 +89,20 @@ const api = {
             throw new Error('Tickers supplied to api.quotes must be in array form.');
         }
     },
+
+    /**
+     * @function api.historical
+     *
+     * Get historical data for a security's past prices.
+     *
+     * @param {string} ticker - the symbol for the security / stock
+     * @param {Object} options - options passed to the historicals API call
+     *    interval - interval between each entry, 'week', 'day', '10minute', '5minute'
+     *    span - how far back the historical records go from today, 'day', 'week', 'year', '5year', 'all'
+     *    bounds - whether to show values for extended trading periods, optional, 'extended', 'regular', 'trading'
+     *
+     * @returns {Promise} - deserialized JSON API response to the request
+     */
     historical: (ticker, {interval, span, bounds} = {}) => {
         const INTERVAL_CHOICES = ['week', 'day', '10minute', '5minute'];
         const SPAN_CHOICES = ['day', 'week', 'year', '5year', 'all'];
@@ -82,6 +128,19 @@ const api = {
             throw new Error('Ticker supplied to api.historical must be a string.');
         }
     },
+    /**
+     * @function api.historicals
+     *
+     * Get historical data for multiple securities' past prices.
+     *
+     * @param {Array:string} tickers - the array of string symbols for securities
+     * @param {Object} options - options passed to the historicals API call
+     *    interval - interval between each entry, 'week', 'day', '10minute', '5minute'
+     *    span - how far back the historical records go from today, 'day', 'week', 'year', '5year', 'all'
+     *    bounds - whether to show values for extended trading periods, optional, 'extended', 'regular', 'trading'
+     *
+     * @returns {Promise} - deserialized JSON API response to the request
+     */
     historicals: (tickers, {interval, span, bounds} = {}) => {
         const INTERVAL_CHOICES = ['week', 'day', '10minute', '5minute'];
         const SPAN_CHOICES = ['day', 'week', 'year', '5year', 'all'];
